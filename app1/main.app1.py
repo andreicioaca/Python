6a82663e315ae937270510a21b32ca3d8525639b
andreicioaca <69766097+andreicioaca@users.noreply.github.com>
2023-07-30 01:55:12 +0300

    Update main.app1.py
    Creating the to do list app!

--- app1/main.app1.py
+++ app1/main.app1.py
@@ -1,11 +1,28 @@
-prompt = 'enter a todo:'
-
 todos = []
 
 while True:
-    todo = input(prompt)
-    todos.append(todo)
-    print(todos)
+    user_action = input("add or show or exit or edit: ")
+    user_action = user_action.strip()
+
+    if user_action == 'add':
+        todo = input("enter a todo: ")
+        todos.append(todo)
+    elif user_action == 'show':
+        for index,item in enumerate(todos):
+            print(index + 1, '-',  item)
+    elif user_action == 'edit':
+        number = int(input("Number of the todo to edit: "))
+        number = number - 1
+        new_todo = input("edit the todo: ")
+        todos[number] = new_todo
+    elif user_action == 'complete':
+        number = int(input("Number of the todo to complete: "))
+        number = number - 1
+        todos.pop(number)
+    elif user_action == 'exit':
+        break
+print('Byeee')
+
 
 
 
