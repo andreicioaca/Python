b377eb473972264e0f1c09bffcab8210cfcbf634
andreicioaca <69766097+andreicioaca@users.noreply.github.com>
2023-07-30 17:12:35 +0300

    Organizing and documenting the code
    Some small changes and documenting the functions of the code

--- app1/main.app1.py
+++ app1/main.app1.py
@@ -1,12 +1,14 @@
-def get_todos(filepath):
+def get_todos(filepath='todos.txt'):
+    """Read a text file and return the list of to_do items"""
     with open(filepath, 'r') as file:
         todos_local = file.readlines()
     return todos_local
 
 
-def push_todos(filepath, todos_arg):
+def push_todos(todos_arg, filepath='todos.txt'):
+    """Write a to_do items in the text file"""
     with open(filepath, 'w') as file:
-            file.writelines(todos_arg)
+        file.writelines(todos_arg)
 
 
 while True:
@@ -17,46 +19,48 @@ while True:
 
         todo = user_action[4:]
 
-        todos = get_todos('todos.txt')
+        todos = get_todos()
 
         todos.append(todo + '\n')
 
-        push_todos('todos.txt', todos)
+        push_todos(todos)
 
     elif user_action.startswith('show'):
 
-        todos = get_todos('todos.txt')
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
             print(index + 1, '-',  item)
 
     elif user_action.startswith('edit'):
+
         try:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos('todos.txt')
+            todos = get_todos()
 
             new_todo = input("edit the todo: ")
             todos[number] = new_todo + '\n'
 
-            push_todos('todos.txt', todos)
+            push_todos(todos)
 
         except ValueError:
             print('command not valid')
             continue
     elif user_action.startswith('complete'):
+
         try:
             number = int(user_action[9:])
 
-            todos = get_todos('todos.txt')
+            todos = get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            push_todos('todos.txt', todos)
+            push_todos(todos)
 
             mess = f"Todo: ___{todo_to_remove}___ was removed from the list"
             print(mess)
@@ -71,7 +75,3 @@ while True:
         print('command not valid')
 
 print('Byeee')
-
-
-
-
